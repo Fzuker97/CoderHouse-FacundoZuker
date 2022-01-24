681f902a7e381169de686e6ff97481fd2a9f65b8
Fzuker97 <86082134+Fzuker97@users.noreply.github.com>
2022-01-24 08:12:57 -0300

    Update main.js

--- Entregable 1/main.js
+++ Entregable 1/main.js
@@ -1,13 +1,28 @@
 function resultado(){
-    let n1 = Number(document.getElementById('Plata').value);
-    let n2 = Number(document.getElementById('Cuotas').value);
-    let n3 = Number(document.getElementById('Interes').value);
-    const resul = ((n1 +((n1*n3)/100))/n2).toFixed(2);
-    if(n1>0 && n2>0 && n3>0){
-        alert('son'+' ' + n2 +" "+ 'Cuotas de' +" $"+ resul);
+    let n1 = parseInt(document.getElementById('Plata').value);
+    let n2 = parseInt(document.getElementById('Cuotas').value);
+    let n3 = parseInt(document.getElementById('Interes').value);
+    const d1 = (n1/n2).toFixed(2);
+    const d2 = (((n1*n3)/100)/n2).toFixed(2);
+    const d3 = ((n1/n2)+(((n1*n3)/100)/n2)).toFixed(2);
+    const d4 = ((n1*n3)/100).toFixed(2);
+    const d5 = (n1+((n1*n3)/100));
+    if(n1>0){
+        for(i=1;i<=n2;i++){
+            document.getElementById("tab").innerHTML= 
+                    `<tr>
+                        <td>${i}</td>
+                        <td>${d1}</td>
+                        <td>${d2}</td>
+                        <td>${d3}</td>
+                    </tr>`;
+        }
+
+        document.getElementById("t1").innerHTML= n1;
+        document.getElementById("t2").innerHTML= d4;
+        document.getElementById("t3").innerHTML= d5;        
     }else{
-        alert('Dato no valido o Dato Faltante.');
+        alert("Falta ingresar un Número");
     }
-    
 }
 
