dc4285178535e5f1f4c91ea37436bd8b21bbd172
Fzuker97 <86082134+Fzuker97@users.noreply.github.com>
2022-03-02 02:46:24 -0300

    Add files via upload

---
+++ Fetch.zuker/main.js
@@ -0,0 +1,127 @@
+let carritoDeCompras = []
+let stock = []
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+
+/*mostrarProductos(stock);  */
+
+fetch('stock.json')
+    .then(Response => Response.json())
+    .then((data) =>{
+        console.log(typeof data);
+        mostrarProductos(data);
+    })
+    .then((data) =>{
+        console.log(typeof data);
+        data=>{stock=data};
+    })
+    .catch((error) => console.log(error));
+
+function mostrarProductos(array){
+    contenedorProductos.innerHTML="";
+    for(const producto of array){
+        let div = document.createElement('div');
+        div.className = 'producto';
+        div.innerHTML += `
+           <div class="card">
+           <span class="card-title">${producto.nombre}</span>
+           <div class="card-content">
+                <img src=${producto.img}>
+               <p> $${producto.precio}</p>
+               <button id="botonAgregar${producto.id}" class="btn">+</button>
+        </div>
+      </div>
+        `
+        contenedorProductos.appendChild(div);
+
+        let btnAgregar = document.getElementById(`botonAgregar${producto.id}`)
+        
+
+        btnAgregar.addEventListener('click', ()=>{
+            console.log(producto.id)
+            agregarAlCarrito(producto.id)
+            Toastify({
+                text: "Producto agregado!",
+                className: "info",
+                style: {
+                  background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+              }).showToast();
+        })
+
+    }
+}
+
+
+
+function agregarAlCarrito(id){
+    let repetido = carritoDeCompras.find(item => item.id == id)
+    if(repetido){
+        console.log(repetido);
+        repetido.cantidad = repedito.cantidad +1
+        document.getElementById(`cantidad${repetido.id}`).innerHTML = `<p id=cantidad${repetido.id}>Cantidad:${repetido.cantidad}</p>`
+        actualizarCarrito()
+    }else{
+
+    //let agregarProducto = stock.find(elemento => elemento.id == id)
+    console.log(typeof id);
+    let agregarProducto = stock.find((elemento) => {
+        console.log(elemento.id, id);
+        if(elemento.id === id){
+            return elemento;
+        }
+    })
+ 
+    carritoDeCompras.push(agregarProducto)
+    actualizarCarrito(carritoDeCompras)
+    console.log(carritoDeCompras);
+
+    let div = document.createElement('div')
+    div.className = 'productoEnCarrito'
+    div.innerHTML =`
+                <p>${agregarProducto.nombre}</p>
+                <p>${agregarProducto.precio}</p>
+                <p id=cantidad${agregarProducto.id}>Cantidad:${agregarProducto.cantidad}</p>
+                <button id=botonEliminar${agregarProducto.id} class="boton-eliminar"><img src="https://img.icons8.com/material-outlined/24/000000/trash--v1.png"/></button>
+    `
+    contenedorCarrito.appendChild(div)
+
+    let botonEliminar = document.getElementById(`botonEliminar${agregarProducto.id}`)
+    botonEliminar.addEventListener('click',()=>{
+        console.log(agregarProducto.id);
+        botonEliminar.parentElement.remove()
+        carritoDeCompras = carritoDeCompras.filter(elemento => elemento.id != agregarProducto.id)
+        actualizarCarrito()
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
+        Toastify({
+            text: "Producto eliminado!",
+            className: "info",
+            style: {
+              background: "linear-gradient(to right, red, orange)",
+            }
+          }).showToast();
+        })
+    }
+    localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
+}
+
+function actualizarCarrito(){
+    contadorCarrito.innerText = carritoDeCompras.reduce((acc,el)=> acc + el.cantidad, 0)
+    precioTotal.innerText = carritoDeCompras.reduce((acc,el)=> acc + (el.precio * el.cantidad), 0)
+
+}
+
+function recuperar(){
+    let recuperarLs = JSON.parse(localStorage.getItem('carrito')) || []
+
+    recuperarLs.forEach(element => {
+        agregarAlCarrito(elemento.id)
+    });
+    
+}
+
+recuperar()
+
---
+++ Fetch.zuker/stock.js
@@ -0,0 +1,21 @@
+/*
+let stock = [
+    {id: 1,nombre:'amd5', tipo:'cpu', precio: 4000,img:'/img/amd5.png', cantidad: 1},
+    {id: 2,nombre:'amd7', tipo:'cpu', precio: 8000,img:'/img/amd7.png', cantidad: 1},
+    {id: 3,nombre:'intel5', tipo:'cpu', precio: 5000,img:'/img/intel5.png', cantidad: 1},
+    {id: 4,nombre:'intel7', tipo:'cpu', precio: 9000,img:'/img/intel7.png', cantidad: 1},
+    {id: 5,nombre:'h510m', tipo:'motherboard', precio: 3000,img:'/img/MB-h510m.png', cantidad: 1},
+    {id: 6,nombre:'b450', tipo:'motherboard', precio: 7500,img:'/img/MB-b450.png', cantidad: 1},
+    {id: 7,nombre:'8gb', tipo:'ram', precio: 4000,img:'/img/ram8gb.png', cantidad: 1},
+    {id: 8,nombre:'16gb', tipo:'ram', precio: 7000,img:'/img/ram16gb.png', cantidad: 1},
+    {id: 9,nombre:'32gb', tipo:'ram', precio: 12000,img:'/img/ram32gb.png', cantidad: 1},
+    {id: 10,nombre:'gtx1050', tipo:'gpu', precio: 8000,img:'/img/gtx1050.png', cantidad: 1},
+    {id: 11,nombre:'rx580', tipo:'gpu', precio: 18000,img:'/img/rx580.png', cantidad: 1},
+    {id: 12,nombre:'rtx3060', tipo:'gpu', precio: 32000,img:'/img/rtx3060.png', cantidad: 1},
+    {id: 13,nombre:'600w', tipo:'fuente', precio: 3000,img:'/img/fuente600w.png', cantidad: 1},
+    {id: 14,nombre:'850w', tipo:'fuente', precio: 8000,img:'/img/fuente850w.png', cantidad: 1},
+    {id: 15,nombre:'1000w', tipo:'fuente', precio: 15000,img:'/img/fuente1000w.png', cantidad: 1},
+]
+
+*/
+
